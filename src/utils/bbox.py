d4086df3bc0e4968ddbb4f409c5f171868991e0c
BlueDruddigon <haibuihoang1999@gmail.com>
2024-02-16 17:37:30 +0700

    docs(utils): add function's documentation

--- src/utils/bbox.py
+++ src/utils/bbox.py
@@ -2,7 +2,13 @@ import torch
 
 
 def dist2bbox(distances: torch.Tensor, anchor_points: torch.Tensor, bbox_format: str = 'xyxy') -> torch.Tensor:
-    """Transform distance(ltrb) to bbox(xyxy or xywh)"""
+    """Transform distance(ltrb) to bbox(xyxy or xywh)
+
+    :param distances: distances from center point to actual bounding box
+    :param anchor_points: anchor's center points coordinates
+    :param bbox_format: returned bounding boxes format
+    :return: bounding boxes tensor
+    """
     lt, rb = torch.split(distances, 2, dim=-1)
     x1y1 = anchor_points - lt
     x2y2 = anchor_points + rb
@@ -14,8 +20,14 @@ def dist2bbox(distances: torch.Tensor, anchor_points: torch.Tensor, bbox_format:
     return torch.cat([c_xy, wh], dim=-1)
 
 
-def bbox2dist(anchor_points: torch.Tensor, bbox: torch.Tensor, reg_max: float):
-    """Transform bbox(xyxy) to distance(ltrb)"""
+def bbox2dist(anchor_points: torch.Tensor, bbox: torch.Tensor, reg_max: int) -> torch.Tensor:
+    """Transform bbox(xyxy) to distance(ltrb)
+
+    :param anchor_points: anchor's center points coordinates
+    :param bbox: bounding boxes coordinates
+    :param reg_max: maximum allowed value for predictions in regression branch
+    :return: distances from anchor points to bounding boxes
+    """
     x1y1, x2y2 = torch.split(bbox, 2, dim=-1)
     lt = anchor_points - x1y1
     rb = x2y2 - anchor_points
@@ -24,7 +36,11 @@ def bbox2dist(anchor_points: torch.Tensor, bbox: torch.Tensor, reg_max: float):
 
 
 def xywh2xyxy(bbox: torch.Tensor) -> torch.Tensor:
-    """Convert bbox from xywh to xyxy format"""
+    """convert bbox from xywh to xyxy format
+
+    :param bbox: a bounding box tensor to be converted.
+    :return: converted tensor.
+    """
     bbox[..., 0] = bbox[..., 0] - bbox[..., 2] * 0.5
     bbox[..., 1] = bbox[..., 1] - bbox[..., 3] * 0.5
     bbox[..., 2] = bbox[..., 0] + bbox[..., 2]
